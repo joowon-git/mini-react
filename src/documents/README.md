c825ea911434556cb8805ed46a6ddace483a19f9
joowon-git <kim5638yw@gmail.com>
2024-10-11 14:54:35 +0900

    사용하지 않는 import 주석

---
+++ src/documents/README.md
@@ -0,0 +1,11 @@
+# Netlify 배포
+## 수동적인 배포
+- npm install netlify-cli -g
+- netlify deploy
+
+
+## Netlify & Github 연동으로 자동 배포
+- Netlify에 프로젝트 생성
+- Github 연동
+- commit 할 때마다 Build
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import MarketJSX from './components/MarketJSX';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import MarketJSX from "./components/MarketJSX";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MarketJSX />
